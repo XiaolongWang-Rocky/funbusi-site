44322c8c501845c6d3486e8736ba006ac8c70b0e
Rocky Wang <wangrocky89@gmail.com>
2023-01-27 22:06:54 +1100

    circle drawing solved

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,29 +7,29 @@ function Services() {
                 </h1>
                 <div className="row funbusi_cards_row">
                     <div className="col-md-4 funbusi_card_container">
-                        <svg>
-                            <circle className="wow animated"></circle>
+                        <svg className="funbusi_circle_svg">
+                            <circle className="wow funbusi_circle animated"></circle>
                         </svg>
-                        <div className="card funbusi_card" data-wow-duration="2s" data-wow-delay="0.5s">
+                        <div className="card funbusi_card zoomIn animated wow">
                             <h3>Business<br />Solutions</h3>
                         </div>
                         <p>We provide comprehensive business solutions for your business, including your POS system,
                             alert and surveillance system</p>
                     </div>
                     <div className="col-md-4 funbusi_card_container">
-                        <svg>
-                            <circle className="wow animated"></circle>
+                        <svg className="funbusi_circle_svg">
+                            <circle className="wow funbusi_circle animated"></circle>
                         </svg>
-                        <div className="card funbusi_card" data-wow-duration="2s" data-wow-delay="0.5s">
+                        <div className="card funbusi_card zoomIn animated wow">
                             <h3>POS<br />System</h3>
                         </div>
                         <p>Choose whom? No worries, we’ll advice you the most suitable</p>
                     </div>
                     <div className="col-md-4 funbusi_card_container">
-                        <svg>
-                            <circle className="wow animated"></circle>
+                        <svg className="funbusi_circle_svg">
+                            <circle className="wow funbusi_circle animated"></circle>
                         </svg>
-                        <div className="card funbusi_card" data-wow-duration="2s" data-wow-delay="0.5s">
+                        <div className="card funbusi_card zoomIn animated wow">
                             <h3>Web<br />Design</h3>
                         </div>
                         <p>Our professional design will make your business remarkable</p>
@@ -37,28 +37,28 @@ function Services() {
                 </div>
                 <div className="row funbusi_cards_row">
                     <div className="col-md-4 funbusi_card_container">
-                        <svg>
-                            <circle className="wow animated"></circle>
+                        <svg className="funbusi_circle_svg">
+                            <circle className="wow funbusi_circle animated"></circle>
                         </svg>
-                        <div className="card funbusi_card" data-wow-duration="2s" data-wow-delay="0.5s">
+                        <div className="card funbusi_card zoomIn animated wow">
                             <h3>High<br />Efficiency</h3>
                         </div>
                         <p>Make your business easy</p>
                     </div>
                     <div className="col-md-4 funbusi_card_container">
-                        <svg>
-                            <circle className="wow animated"></circle>
+                        <svg className="funbusi_circle_svg">
+                            <circle className="wow funbusi_circle animated"></circle>
                         </svg>
-                        <div className="card funbusi_card" data-wow-duration="2s" data-wow-delay="0.5s">
+                        <div className="card funbusi_card zoomIn animated wow">
                             <h3>Fast<br />Response</h3>
                         </div>
                         <p>Time is money</p>
                     </div>
                     <div className="col-md-4 funbusi_card_container">
-                        <svg>
-                            <circle className="wow animated"></circle>
+                        <svg className="funbusi_circle_svg">
+                            <circle className="wow funbusi_circle animated"></circle>
                         </svg>
-                        <div className="card funbusi_card" data-wow-duration="2s" data-wow-delay="0.5s">
+                        <div className="card funbusi_card zoomIn animated wow">
                             <h3>Good<br />Quality</h3>
                         </div>
                         <p>No quality, no deal</p>
