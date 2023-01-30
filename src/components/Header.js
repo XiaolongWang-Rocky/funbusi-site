0b3b04641a3e4ed00270fde42bc3eb4da9571b2d
Rocky Wang <wangrocky89@gmail.com>
2023-01-30 12:21:41 +1100

    responsive dropdown menu completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import logo from '../images/logo.png';
+import { MenuOutlined } from '@ant-design/icons';
+import { navbarItems } from '../static-data';
+import { Dropdown, Space } from 'antd';
 
 function Header() {
     return (
@@ -7,7 +10,7 @@ function Header() {
                 <a id="home" href="/#banner" className="scroll"><img alt="FUNBUSI Logo" src={logo} /></a>
             </div>
             <div className="col-md-6">
-                <ul className="nav nav-pills funbusi_nav">
+                {/* <ul className="nav nav-pills funbusi_nav">
                     <li className="nav-item funbusi_nav_btn">
                         <a className="nav-link scroll" href="/#services_sec">Services</a>
                     </li>
@@ -19,12 +22,32 @@ function Header() {
                     </li>
                     <li className="nav-item dropdown ml-md-auto">
                         <a className="nav-link dropdown-toggle funbusi_menu_drop" href="http://example.com"
-                            id="navbarDropdownMenuLink" data-toggle="dropdown"></a>
+                            id="navbarDropdownMenuLink" data-toggle="dropdown"><MenuOutlined /></a>
                         <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
                             <a className="dropdown-item scroll" href="#services_sec">Services</a>
                             <a className="dropdown-item scroll" href="#about_us">About Us</a>
                             <a className="dropdown-item scroll" href="#contact_us">Contact Us</a>
                         </div>
+                    </li>
+                </ul> */}
+                <ul className="nav nav-pills funbusi_nav">
+                    <li className="nav-item funbusi_nav_btn">
+                        <a className="nav-link scroll" href="/#services_sec">Services</a>
+                    </li>
+                    <li className="nav-item funbusi_nav_btn">
+                        <a className="nav-link scroll" href="/#about_us">About Us</a>
+                    </li>
+                    <li className="nav-item funbusi_nav_btn">
+                        <a className="nav-link scroll" href="/#contact_us">Contact Us</a>
+                    </li>
+                    <li className="nav-item dropdown ml-md-auto funbusi_menu_drop">
+                        <Dropdown menu={{items: navbarItems}}>
+                            <a onClick={e=>e.preventDefault}>
+                                <Space>
+                                    <MenuOutlined style={{fontSize: '28px'}}/>
+                                </Space>
+                            </a>
+                        </Dropdown>
                     </li>
                 </ul>
             </div>
---
+++ src/static-data.js
@@ -0,0 +1,20 @@
+export const navbarItems = [
+    {
+        key: 'services',
+        label: (
+            <a rel="noopener norererrer" href="/#services_sec" className="funbusi_dropdown_menu_item scroll">Services</a>
+        )
+    },
+    {
+        key: 'aboutUs',
+        label: (
+            <a rel="noopener norererrer" href="/#about_us" className="funbusi_dropdown_menu_item scroll">About Us</a>
+        )
+    },
+    {
+        key: 'contactUs',
+        label: (
+            <a rel="noopener norererrer" href="/#contact_us" className="funbusi_dropdown_menu_item scroll">Contact Us</a>
+        )
+    },
+];
