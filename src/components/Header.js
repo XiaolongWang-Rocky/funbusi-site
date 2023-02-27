87a4c8403d240c12d6f8245a8fb52bafc8a879ad
Rocky Wang <wangrocky89@gmail.com>
2023-02-27 15:07:00 +1100

    modified footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,26 +10,6 @@ function Header() {
                 <a id="home" href="/#banner" className="scroll"><img alt="FUNBUSI Logo" src={logo} /></a>
             </div>
             <div className="col-md-6">
-                {/* <ul className="nav nav-pills funbusi_nav">
-                    <li className="nav-item funbusi_nav_btn">
-                        <a className="nav-link scroll" href="/#services_sec">Services</a>
-                    </li>
-                    <li className="nav-item funbusi_nav_btn">
-                        <a className="nav-link scroll" href="/#about_us">About Us</a>
-                    </li>
-                    <li className="nav-item funbusi_nav_btn">
-                        <a className="nav-link scroll" href="/#contact_us">Contact Us</a>
-                    </li>
-                    <li className="nav-item dropdown ml-md-auto">
-                        <a className="nav-link dropdown-toggle funbusi_menu_drop" href="http://example.com"
-                            id="navbarDropdownMenuLink" data-toggle="dropdown"><MenuOutlined /></a>
-                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                            <a className="dropdown-item scroll" href="#services_sec">Services</a>
-                            <a className="dropdown-item scroll" href="#about_us">About Us</a>
-                            <a className="dropdown-item scroll" href="#contact_us">Contact Us</a>
-                        </div>
-                    </li>
-                </ul> */}
                 <ul className="nav nav-pills funbusi_nav">
                     <li className="nav-item funbusi_nav_btn">
                         <a className="nav-link scroll" href="/#services_sec">Services</a>
